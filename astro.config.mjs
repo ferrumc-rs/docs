1a39a17cc83247e79263564433d77466b4563481
Skullian <133133667+Skullians@users.noreply.github.com>
2024-11-08 00:37:57 +0000

    Pray that this fixes 404s

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ferrumc-rs.github.io',
+	site: 'https://ferrumc-rs.github.io/',
         base: 'docs',
 	integrations: [
 		starlight({
