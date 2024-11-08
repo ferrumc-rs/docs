2ade731ef72a4a58e83589c5ffd2ebd0e7df58e8
Skullian <133133667+Skullians@users.noreply.github.com>
2024-11-08 00:35:07 +0000

    Fix 404s while on default gh pages url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://ferrumc-rs.github.io',
+        base: 'docs',
 	integrations: [
 		starlight({
 			title: 'FerrumC Docs',
