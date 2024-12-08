dfba9b0696e6f9ec97a86be84c3a4a858d4454c1
ReCore <kjshore2004@gmail.com>
2024-12-08 13:27:06 +1030

    Added caching docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,43 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'FerrumC Docs',
-			social: {
-				discord: 'https://discord.gg/qT5J8EMjwk',
-				github: 'https://github.com/ferrumc-rs/ferrumc',
-			},
-			sidebar: [
-				{
-					label: 'Start Here',
-					items: [
-						{ label: 'Getting Started', slug: 'start/getting_started' },
-						{ label: 'Migrate', slug: 'start/migrate' },
-					],
-				},
-				{
-					label: 'About',
-					items: [
-						{label: 'Introduction', slug: 'about/introduction'},
-						{label: 'FAQ', slug: 'about/faq'},
-						{label: 'Comparison', slug: 'about/comparison'},
-					],
-				},
-				{
-					label: "Internal Documentation",
-					items: [
-						{label: 'Getting Started', slug: 'internal/getting_started'},
-                        {label: 'Database', slug: 'internal/database'},
-					],
-				}
-			],
-			favicon: "/favicon.ico",
-			customCss: [
-				'src/style/theme.css',
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "FerrumC Docs",
+      social: {
+        discord: "https://discord.gg/qT5J8EMjwk",
+        github: "https://github.com/ferrumc-rs/ferrumc",
+      },
+      sidebar: [
+        {
+          label: "Start Here",
+          items: [
+            { label: "Getting Started", slug: "start/getting_started" },
+            { label: "Migrate", slug: "start/migrate" },
+          ],
+        },
+        {
+          label: "About",
+          items: [
+            { label: "Introduction", slug: "about/introduction" },
+            { label: "FAQ", slug: "about/faq" },
+            { label: "Comparison", slug: "about/comparison" },
+          ],
+        },
+        {
+          label: "Internal Documentation",
+          items: [
+            { label: "Getting Started", slug: "internal/getting_started" },
+            { label: "Database", slug: "internal/database" },
+            { label: "Caching", slug: "internal/caching" },
+          ],
+        },
+      ],
+      favicon: "/favicon.ico",
+      customCss: ["src/style/theme.css"],
+    }),
+  ],
 });
